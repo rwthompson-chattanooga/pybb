67025dd936d5c792b90f70f1c2a87745e2969ff2
Richard Thompson <rwthompson2493@gmail.com>
2026-05-15 14:33:41 -0400

    BAL breaks this code, but it works with other teams. See if I can figure it out.

--- teamsched.py
+++ teamsched.py
@@ -2,7 +2,11 @@ from pybaseball import schedule_and_record
 from pybaseball import standings
 
 import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
 import numpy as np  
+
+currseason = pd.DataFrame()
+#print(currseason.head(25))
 
 teamlist = [
     'ARI', 'ATL', 'BAL', 'BOS', 'CHC', 'CIN', 'CLE', 'COL', 'DET', 'HOU',
@@ -10,14 +14,17 @@ teamlist = [
     'SD', 'SEA', 'SF', 'STL', 'TB', 'TEX', 'TOR', 'WSH' 
 ]
 
-#print(teamlist)
+currseason = pd.concat([currseason, schedule_and_record(2026, 'BAL')])
+    
 
+print(currseason.head(5)) 
 
-""" for team in teamlist:
-    print(team) """
-        
-atlsched = schedule_and_record({2026}, {'ATL'})
+""" for x in range(0, len(teamlist)):
+    currseason = pd.concat([currseason, schedule_and_record(2026, teamlist[x])]) """
 
-print(atlsched.head(5))
+print(currseason.shape)
+#atlsched = schedule_and_record(2026, 'ATL')
+
+#print(atlsched.head(5))
 #print(atlsched.iloc[:, 0:4])
 #print(type(atlsched.iloc[1,0]))
